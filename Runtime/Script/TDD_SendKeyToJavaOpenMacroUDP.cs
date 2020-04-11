c5ae0c047ba1599879f8aed02d56b175cfbac081
streeeloi <streeeloi@gmail.com>
2020-04-11 02:13:32 +0200

    Save first commit

---
+++ Runtime/Script/TDD_SendKeyToJavaOpenMacroUDP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDD_SendKeyToJavaOpenMacroUDP : MonoBehaviour
+{
+    public JavaOpenMacroUDP m_udpSender;
+    private void Start()
+    {
+        InvokeRepeating("TDD_SendMSG", 0, 2);
+      
+    }
+    public void TDD_SendMSG()
+    {
+        m_udpSender.Send((JavaKeyEvent)UnityEngine.Random.Range(0, 80), JavaOpenMacroUDP.PressType.Stroke);
+    }
+}
