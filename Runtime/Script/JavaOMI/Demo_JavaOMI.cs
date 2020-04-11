c9b2ce2743b0631bd6dbfe7695757119626e4791
streeeloi <streeeloi@gmail.com>
2020-04-11 19:30:55 +0200

    Save: 04/11/2020 7:30 PM

---
+++ Runtime/Script/JavaOMI/Demo_JavaOMI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo_JavaOMI : MonoBehaviour
+{
+    public JavaOMI deviceTarget;
+    public JavaKeyEvent[] m_toSend = new JavaKeyEvent[] {JavaKeyEvent.VK_O, JavaKeyEvent.VK_K };
+    public float m_delayTime = .1f;
+
+    IEnumerator Start()
+    {
+        deviceTarget = JavaOMI.CreateDefaultOne();
+
+        while (true) {
+            yield return new WaitForSeconds(3);
+            for (int i = 0; i < m_toSend.Length; i++)
+            {
+                deviceTarget.Keyboard(m_toSend[i]);
+                yield return new WaitForSeconds(m_delayTime);
+
+            }
+        }
+    }
+    public void Reset()
+    {
+        JavaOMI.TryBasicDirtyConvertion("Hello World", out m_toSend);
+    }
+
+}
